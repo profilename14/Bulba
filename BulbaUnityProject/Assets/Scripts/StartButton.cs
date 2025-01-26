8b64361051fece5f77169fe0d2a4e3e9c1f3c187
Jim Niehues <3d.smith@gmail.com>
2025-01-25 16:33:30 -0800

    Main Menu

---
+++ BulbaUnityProject/Assets/Scripts/StartButton.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    public Button button;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        button.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        Debug.Log("Starting");
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+        SceneManager.LoadScene(2, LoadSceneMode.Additive);
+    }
+}
