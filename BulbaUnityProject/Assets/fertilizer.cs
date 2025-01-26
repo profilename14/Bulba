e59b266f11386cf9fee79a671648ccef654752a2
James Jasper Fadden O'Roarke <faddenjasper@gmail.com>
2025-01-26 13:43:03 -0800

    Fixed fertilizer modifier, added research and fertilizer prefabs

---
+++ BulbaUnityProject/Assets/fertilizer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class fertilizer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D playerCollider)
+    {
+        if (playerCollider.gameObject.tag == "Player")
+        {
+            StatsSingleton.Instance.fertilizer += 50;
+            Destroy(gameObject);
+        }
+    }
+}
